3bbc7642e776e819756acd38e23a36c63a2bf557
Ashlin Mirsha R K <ashlinmirsha@karunya.edu.in>
2026-08-15 15:05:56 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -0,0 +1,15 @@
+# Last updated: 8/15/2026, 3:05:55 PM
+class Solution:
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+
+        original = x
+        reverse = 0
+
+        while x > 0:
+            digit = x % 10
+            reverse = reverse * 10 + digit
+            x = x // 10
+
+        return original == reverse
