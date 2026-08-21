fb84f203e95a476d8ae515050690ef3e23e3905c
Ashlin Mirsha R K <ashlinmirsha@karunya.edu.in>
2026-08-21 15:26:29 +0530

    Create 232-ImplementQueueUsingStacks

---
+++ 232-ImplementQueueUsingStacks/232-ImplementQueueUsingStacks.py
@@ -0,0 +1,27 @@
+# Last updated: 8/21/2026, 3:26:27 PM
+print("Ashlin Mirsha R K")
+print("URK25CS1193")
+class MyQueue:
+
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+
+    def push(self, x):
+        self.in_stack.append(x)
+
+    def pop(self):
+        self.move_elements()
+        return self.out_stack.pop()
+
+    def peek(self):
+        self.move_elements()
+        return self.out_stack[-1]
+
+    def empty(self):
+        return len(self.in_stack) == 0 and len(self.out_stack) == 0
+
+    def move_elements(self):
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
