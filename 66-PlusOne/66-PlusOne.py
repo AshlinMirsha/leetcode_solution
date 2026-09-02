480a114d3be4b6186f892ceff2022d0f44366391
Ashlin Mirsha R K <ashlinmirsha@gmail.com>
2026-09-02 17:18:38 +0530

    File updated at 9/2/2026, 5:18:37 PM

--- 66-PlusOne/66-PlusOne.py
+++ 66-PlusOne/66-PlusOne.py
@@ -1,10 +1,10 @@
-# Last updated: 9/1/2026, 10:23:13 PM
+# Last updated: 9/2/2026, 5:18:37 PM
 class Solution:
-    def plusOne(self,digit):
-        for i in range (len(digit) -1 , -1, -1):
+    def plusOne(self, digit):
+        for i in range (len(digit)-1,-1,-1):
             if digit[i]<9:
                 digit[i]+=1
                 return digit
             digit[i]=0
-        return[1]+digit
-
+        return [1] + digit
+        
